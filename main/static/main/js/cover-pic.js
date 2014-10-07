85c95100a2547ac3214a414d790ac35cc4046aed
Brian Su <briansudev@gmail.com>
2014-10-07 12:21:44 -0700

    changed main banner pic to scroll

--- main/static/main/js/cover-pic.js
+++ main/static/main/js/cover-pic.js
@@ -2,7 +2,7 @@ function loadCoverPic(container, static_url) {
   var photos = ['balloon', 'water', 'mountains', 'houses', 'concert'];
   var randomImage = photos[Math.floor(Math.random() * photos.length)];
   var imageURL = 'main/img/banner/' + randomImage + '.jpg';
-  $(container).css("background", "url(" + static_url + imageURL + ") no-repeat center center fixed");
+  $(container).css("background", "url(" + static_url + imageURL + ") no-repeat center center scroll");
   $(container).css("background-size", "cover");
   $(container).css("-webkit-background-size", "cover");
   $(container).css("-moz-background-size", "cover");
