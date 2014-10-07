d78cf8be38bae795713614db1376bea4cc429211
Brian Su <briansudev@gmail.com>
2014-10-06 20:09:58 -0700

    added more banner pics

---
+++ main/static/main/js/cover-pic.js
@@ -0,0 +1,10 @@
+function loadCoverPic(container, static_url) {
+  var photos = ['balloon', 'water', 'mountains', 'houses', 'concert'];
+  var randomImage = photos[Math.floor(Math.random() * photos.length)];
+  var imageURL = 'main/img/banner/' + randomImage + '.jpg';
+  $(container).css("background", "url(" + static_url + imageURL + ") no-repeat center center fixed");
+  $(container).css("background-size", "cover");
+  $(container).css("-webkit-background-size", "cover");
+  $(container).css("-moz-background-size", "cover");
+  $(container).css("-o-background-size", "cover");
+}
